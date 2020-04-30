a03ef6b19c8a65fa24ece4e3e455fff6507d3aa2
yamagen612 <20yamagen@nao.net.it-chiba.ac.jp>
2020-04-30 21:32:33 +0900

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<meat charset='utf-8'>
+
+<body>
+    <h1><?php echo "PHPから表示";?></h1>
+    <?php
+        $str_hello = "HTML内で"
+        echo "<p> $str_hello PHPスクリプトを表示してみたよ</p>";
+        ?>
+</body>
+</html>
