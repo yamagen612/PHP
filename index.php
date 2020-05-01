da8c69fe15e33001cb086dbcfc84674cf2660042
yamagen612 <63621420+yamagen612@users.noreply.github.com>
2020-05-01 16:30:04 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <!DOCTYPE html>
 <html>
 <head>
-
 <meat charset='utf-8'>
-
+    <title>HTML内でのPHPスクリプト　</title>
+</head>
+    
 <body>
     <h1><?php echo "PHPから表示";?></h1>
     <?php
@@ -11,5 +12,5 @@
         echo "<p> $str_hello PHPスクリプトを表示してみたよ</p>";
         ?>
 </body>
-</head>
+
 </html>
